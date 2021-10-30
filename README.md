c69c114d5bd6b0b46e0357a9fb67ab79355f2f42
Daniel Hakim <daniel.hakim.jamaluddin@gmail.com>
2021-10-30 15:31:22 +0800

    Add condition for winnning players and if the game is draw

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Ruby_Project-Tic_Tac_Toe
+
 * This a project of ruby involving the use of object oriented programming.
 * I know, this project will not be easy for me but I must do it in order to complete my The Odin Project course.
 * Lately, I have temptation to give up on this and do something else, but I choose to not give up.
+
+Click [here]("Tic Tac Toe") to go to the replit.com to test the code.
 ---
 25/10/21
 * I know to do some of the basics funcionality but I still need time to get the flow of the program.
@@ -15,4 +18,12 @@
 * Finally, most of the error handling if user gives wrong input for marker has been solved and handled properly.
 * One last funcitonality is to build condition of winners or draw is none of the users obtain straight row marks. There are 8 conditions could lead to win .
 * The way I solved it by breaking down the game into smaller pieces and solve it one by one. It's also important to know the rough flow of the overall game.
+---
+30/10/21
+* Add condition to win and draw for the game.
+* The minimum functionality for the game has been completed.
+* However, some improvement can be made including
+  - [] Random first turn for the players.
+  - [] Flexibility of using different markers.
+  - [] Repeat the game (loop again) when the game has found the winner or the game is draw.
 ---
--- Tic_Tac_Toe.rb
+++ Tic_Tac_Toe.rb
@@ -1,67 +1,134 @@
+
 class Player
   attr_reader :sym, :name
   @@total_player = 0
+
   def initialize(name)
     @name = name
     @@total_player += 1
-    @sym = @@total_player == 1? 'X' : 'O'
+    @sym = @@total_player == 1 ? 'X' : 'O'
+    @row_container = [0, 0, 0]
+    @col_container = [0, 0, 0]
+    @rightdiag_container = [0, 0, 0]
+    @leftdiag_container = [0, 0, 0]
     puts "Your marker will be '#{sym}'."
   end
-  def play()
+
+  def play
     print "\n#{name}, please enter position to mark: "
     target = gets.chomp.to_i
     target = Board.check(target)
-    Board.update(target, sym)
+    coor_arr = Board.update(target, sym)
     Board.display
+    check_win(coor_arr.first, coor_arr.last)
   end
+
   def self.total
     @@total_player
   end
+
+  def check_win(row, col)
+    @row_container[row] += 1
+    @col_container[col] += 1
+    @rightdiag_container[row] += 1 if row == col
+    @leftdiag_container[row] += 1 if row + col + 1 == 3
+    total_rightdiag = @rightdiag_container.reduce(0) { |total_sum, num| total_sum + num }
+
+    total_leftdiag = @leftdiag_container.reduce(0) { |total_sum, num| total_sum + num }
+
+    if @row_container[row] == 3
+      puts "#{name}, you win!"
+      return 1
+    end
+
+    if @col_container[col] == 3
+      puts "#{name}, you win!"
+      return 1
+    end
+
+    if total_rightdiag == 3
+      puts "#{name}, you win!"
+      return 1
+    end
+
+    if total_leftdiag == 3
+      puts "#{name}, you win!"
+      return 1
+    end
+    0
+  end
+
 end
 
 class Board
-  @@board_display = {
-    :row1 => {1 =>'1', 2 =>'2', 3 =>'3'},
-    :row2 => {4 =>'4', 5 =>'5', 6 =>'6'},
-    :row3 => {7 =>'7', 8 =>'8', 9 =>'9'}
-  }
+  @@board_display = [
+    %w[1 2 3],
+    %w[4 5 6],
+    %w[7 8 9]
+  ]
+
   @@previous_input = []
+
   def self.display
     puts
-    @@board_display.each do |key, row|
-      puts "  |    |    |  "
-      row.each {|k, item| print "--#{item}--"}
+    @@board_display.each do |row|
+      puts ' +---+--+---+--+---+ '
+      row.each { |item| print " | #{item} | " }
       puts
     end
-    puts "  |    |    |  \n"
+    puts " +---+--+---+--+---+ \n"
   end
 
   def self.update(target, sym)
-    @@board_display.each do |key, row|
-      row.each do |k, v|
-        if(k == target)
-          row[k] = sym
-          @@previous_input.push(target)
-        end
+    @@coordinate = []
+    @@board_display.each_with_index do |row, i|
+      row.each_with_index do |item, j|
+        next unless item == target.to_s
+        row[j] = sym
+        @@previous_input.push(target)
+        @@coordinate.push(i, j)
       end
     end
+    @@coordinate
   end
 
   def self.check(target)
-    until target >= 1 and target <= 9
+    until (target >= 1) && (target <= 9)
       print "\nInvalid input, Please enter position 0-9: "
       target = gets.chomp.to_i
     end
 
-    any_previous = @@previous_input.any? {|input| input == target}
+    any_previous = @@previous_input.any? { |input| input == target }
 
     while any_previous == true
       puts "\nInvalid input! This position has been already inserted."
-      print "Please enter position available position 0-9: "
+      print 'Please enter position available position 0-9: '
       target = gets.chomp.to_i
-      any_previous = @@previous_input.any? {|input| input == target}
+      any_previous = @@previous_input.any? { |input| input == target }
     end
     target
+  end
+
+  def self.is_draw
+    if @@previous_input.length == 9
+      puts "\nThe game is draw.\n"
+      return 1
+    end
+    0
+  end
+
+  def self.start(player1, player2)
+    Board.display
+    loop do
+      win = player1.play
+      break if win > 0          #check for win condition for player 1
+      draw = Board.is_draw
+      break if draw > 0         #check for draw condition for the game if player 1 makes the last move.
+      win = player2.play
+      break if win > 0          #check for win condition for player 2
+      draw = Board.is_draw
+      break if draw > 0         #check for draw condition for the game if player 2 makes the last move.
+    end
   end
 
 end
@@ -71,15 +138,7 @@ def ask_name(number)
   name = gets.chomp
 end
 
-puts "-----Welcome to the Tic-Tac-Toe-----"
-player1 =Player.new(name = ask_name(Player.total + 1))
-player2 =Player.new(name = ask_name(Player.total + 1))
-
-winner = 0
-Board.display
-
-while (winner == 0)
-  player1.play
-  player2.play
-end
-
+puts '-----Welcome to the Tic-Tac-Toe-----'
+player1 = Player.new(name = ask_name(Player.total + 1))
+player2 = Player.new(name = ask_name(Player.total + 1))
+Board.start(player1, player2)
